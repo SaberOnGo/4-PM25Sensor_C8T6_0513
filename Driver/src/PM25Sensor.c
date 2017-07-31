5f30e6be9503710e87bef506db87259836d0e0bd
SaberOnGo <dear_pi@126.com>
2017-07-31 11:23:10 +0800

    new branch as newest backup version

--- Driver/src/PM25Sensor.c
+++ Driver/src/PM25Sensor.c
@@ -713,10 +713,10 @@ static void TimerCheckPM25_CallBack(void * arg)
            {
                static uint32_t sec = 0;
 
-			   if(OS_SetTimeout(sec) && (OS_GetSysTick() > SEC(15 * 60)))  // 开机后 15分钟开始记录
+			   if(OS_SetTimeout(sec) && (OS_GetSysTick() > SEC(5 * 60)))  // 开机后 15分钟开始记录
 			   {
 			       os_printf("save pm2.5 = %d ug/m3, tick = %ld s\n", mass_fraction, OS_GetSysTick() / 100);
-			       sec = OS_SetTimeout(SEC(5 * 60));  // 5 分钟记录一次
+			       sec = OS_SetTimeout(SEC(1 * 60));  // 5 分钟记录一次
 			       SDRR_SaveSensorPoint(SENSOR_HCHO, (void *)&mass_fraction);
 			   }
            }
